0372b1d881181bb1b509d115af23b42509661075
Ethan Liu <darthpikakirby@gmail.com>
2015-11-15 10:33:21 -0500

    Added missing template elements

--- Public/js/wikicode.js
+++ Public/js/wikicode.js
@@ -9,17 +9,22 @@ function makeWikiCode() {
   
   // Create Wiki Code, song then file
   swikicode += "| name            = " + $("#songname").val() + "\n";
+  swikicode += "| image           = \n| imagewidth      = \n| originaltitle   = \n| aka             = \n| album           = \n| composer        \n";
   swikicode += "| musician        = [[" + $("#musician").val() + "]]\n";
+  swikicode += "| musician        = \n| arranger        = \n| performer       = \n| remixer         = \n| featuring       = \n| songrelease     = \n";
   swikicode += '| genre           = "' + $("#genre").val() + '"\n}}\n';
   
   fwikicode += "| name            = " + $("#songname").val() + "\n";
+  fwikicode += "| image           = \n| imagewidth      = \n";
   fwikicode += "| levelnumber     = " + $("#levelnumber").val() + "\n";
   fwikicode += "| stepartist      = [[" + $("#stepartist").val() + "]]\n";
   fwikicode += "| filerelease     = " + $("#filereleasedate").val() + "\n";
   fwikicode += "| songlength      = " + $("#songlength").val() + "\n";
   fwikicode += "| difficulty      = " + $("#difficulty").val() + "\n";
   fwikicode += "| nps             = " + $("#nps").val() + "\n";
+  fwikicode += '| category        = "' + $("#category").val() + '"\n';  
   fwikicode += "| bpm             = " + $("#bpm").val() + "\n}}\n";
+
   
   /*wikicode += "| levelnumber     = " + $("#levelnumber").val() + "\n";
   wikicode += "| name            = " + $("#songname").val() + "\n";
