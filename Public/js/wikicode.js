32554214e5a9672e9b4e89be9d471caaf79a2160
RVanliew <RVanliew@cainc.com>
2015-11-15 17:36:42 -0500

    Textarea is now monospace font. Also fixed the name attributes in the form field and added a button for future use.

--- Public/js/wikicode.js
+++ Public/js/wikicode.js
@@ -4,42 +4,38 @@ function makeWikiCode() {
   //var ids = ["levelnumber", "songname", "musician", "genre", "category", 
   //            "stepartist", "filereleasedate", "songlength", "difficulty", "notecount", "nps", "bpm"], wikicode = "";
   
-  // Strings to hold song and file Wiki Code
-  var swikicode = "{{Song\n", fwikicode = "{{File";
+  // Strings to hold song and file Wiki Code.
+  var swikicode = "{{Song\n", fwikicode = "{{File\n";
   
-  // Create Wiki Code, song then file
+  // Create Wiki Code String for the Song Template in the WikiCode.
   swikicode += "| name            = " + $("#songname").val() + "\n";
-  swikicode += "| image           = \n| imagewidth      = \n| originaltitle   = \n| aka             = \n| album           = \n| composer        \n";
+  swikicode += "| image           = \n";
+  swikicode += "| imagewidth      = \n";
+  swikicode += "| originaltitle   = \n";
+  swikicode += "| aka             = \n";
+  swikicode += "| album           = \n";
+  swikicode += "| composer        = \n";
   swikicode += "| musician        = [[" + $("#musician").val() + "]]\n";
-  swikicode += "| musician        = \n| arranger        = \n| performer       = \n| remixer         = \n| featuring       = \n| songrelease     = \n";
-  swikicode += '| genre           = "' + $("#genre").val() + '"\n}}\n';
+  swikicode += "| arranger        = \n";
+  swikicode += "| performer       = \n";
+  swikicode += "| remixer         = \n";
+  swikicode += "| featuring       = \n";
+  swikicode += "| songrelease     = \n";
+  swikicode += "| genre           = " + $("#genre").val() + "\n}}\n";
   
+  // File Template WikiCode.
   fwikicode += "| name            = " + $("#songname").val() + "\n";
-  fwikicode += "| image           = \n| imagewidth      = \n";
+  fwikicode += "| image           = \n";
+  fwikicode += "| imagewidth      = \n";
   fwikicode += "| levelnumber     = " + $("#levelnumber").val() + "\n";
   fwikicode += "| stepartist      = [[" + $("#stepartist").val() + "]]\n";
   fwikicode += "| filerelease     = " + $("#filereleasedate").val() + "\n";
   fwikicode += "| songlength      = " + $("#songlength").val() + "\n";
   fwikicode += "| difficulty      = " + $("#difficulty").val() + "\n";
   fwikicode += "| nps             = " + $("#nps").val() + "\n";
-  fwikicode += '| category        = "' + $("#category").val() + '"\n';  
+  fwikicode += "| category        = " + $("#category").val() + "\n";  
   fwikicode += "| bpm             = " + $("#bpm").val() + "\n}}\n";
-
-  
-  /*wikicode += "| levelnumber     = " + $("#levelnumber").val() + "\n";
-  wikicode += "| name            = " + $("#songname").val() + "\n";
-  wikicode += "| musician        = [[" + $("#musician").val() + "]]\n";
-  wikicode += '| genre           = "' + $("#genre").val() + '"\n';
-  wikicode += '| category        = "' + $("#category").val() + '"\n';
-  wikicode += "| stepartist      = [[" + $("#stepartist").val() + "]]\n";
-  wikicode += "| filerelease     = " + $("#filereleasedate").val() + "\n";
-  wikicode += "| songlength      = " + $("#songlength").val() + "\n";
-  wikicode += "| difficulty      = " + $("#difficulty").val() + "\n";
-  wikicode += "| nps             = " + $("#nps").val() + "\n";
-  wikicode += "| bpm             = " + $("#bpm").val() + "\n";*/
   
   // Update Wiki Code textarea
   $('#wikicode').val(swikicode + fwikicode);
-  
-  return;
 }
